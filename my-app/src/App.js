de1c590a38d7ffbbcc9ed01a25ea3f5a96b9d193
Darius <skyview777@msn.com>
2022-10-07 19:47:43 -0500

    added html to app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, {useState, useEffect } from 'react'
+import ChatBubble from './components/ChatBubble'
+import axios from 'axios';
 
-function App() {
+import './components/Style.css';
 
-  const [backEndData, setBackendData] = useState([{}])
-
+ function App() {
+  
+  const [userID, setBackendData] = useState([{}])
+  //fetching user id
   useEffect(() => {
     fetch("/users/new").then(
       response => response.json()
@@ -14,17 +18,82 @@ function App() {
     )
   }, [])
 
+  const [text, setText] = useState()
+
+
+ 
+  const handleSubmit = event => {
+    console.log('handleSubmit ran')
+    event.preventDefault()
+    console.log('test submission: ', text);
+    
+    let config = {
+      headers: {
+         
+      }
+    }
+    const userSubmit = {
+      sender: userID.users,
+      message: text
+      
+    }
+
+    
+    axios.post('http://localhost:5005/webhooks/rest/webhook', userSubmit,config)
+    .then(function (response) {
+          console.log(JSON.stringify(response.data))
+    })
+    
+  }
+
+  
   
   return(
-  //  <div>
-  //   <p>{backEndData.users}</p>
-  //  </div>
-  <body>
-    <div>
-      
+    // <div>
+    //  <p>{userID.users}</p>
+    // </div>
+
+<body>
+    <div class="mainPage"> 
+
+        <div class="title">
+            <span> Team 4 Chat Bot </span>
+        </div>
+
+        <div class="chatHistory">
+            <ul class="chatList">
+                <li class="botOutput botOutput--standard">Hello, I am chatbot 4</li>
+                <li class="botOutput botOutput--standard">I can help</li>
+                <li class="botOutput botOutput--standard">
+                
+
+                    <span class="botOutput--second-sentence">You can ask me anything</span>
+                    <ul>
+                        <li class="input__nested-list">User ID: {userID.users}</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+        <div class="chatboxArea">
+
+            <div class="calculator">
+                <a href="https://www.desmos.com/scientific">Calculator</a>
+            </div>
+
+            <div>
+              <form action="" id="chatform" onSubmit={handleSubmit}>
+                <textarea placeholder="..." class="chatbox" name="chatbox" onChange={event => setText(event.target.value)}  minLength = "2"  style ={{resize:"none"}}></textarea>
+                <input class="submit-button" type="submit" value = "send"    style ={{resize:"none"}}/> 
+              </form>
+             </div>
+
+        </div>
+
+
+
     </div>
-  </body>
- 
+</body>
    
   )
 }
---
+++ my-app/src/components/ChatBubble.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'
+import "./Style.css"
+
+const ChatBubble = () =>{
+    
+
+
+
+    return(
+        // {/*bot message*/}
+        //   <li class = "botOutput botOutput --standard">
+        //     test
+        //   </li>
+              
+      <div>
+        
+      </div>
+    
+    )
+
+}
+export default ChatBubble
