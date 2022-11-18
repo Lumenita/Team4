59869d92ed453d1d2ba8fe9169f12da2e9c8b331
Lumenita <61290963+Lumenita@users.noreply.github.com>
2022-11-17 18:50:57 -0600

    changes applied to app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -56,46 +56,46 @@ import './components/Style.css';
 
     <body>
 
-        <div class="mainPage">
-          <div class="title">
-            <span> Team 4 Chat Bot </span>
-          </div>
+      <div class="mainPage">
+        <div class="title">
+          <span> Team 4 Chat Bot </span>
+        </div>
 
-          <div class="chatHistory">
-            <ul class="chatList">
-              <li class="botOutput botOutput--standard">Hello, I am chatbot 4</li>
-              <li class="botOutput botOutput--standard">I can help</li>
-              <li class="botOutput botOutput--standard">
-                <span class="botOutput--second-sentence">You can ask me anything</span>
-                <ul>
-                  <li class="input__nested-list">User ID: {userID.users}</li>
-                </ul>
-              </li>
-            </ul>
-          </div>
+        <div class="chatHistory">
+          <ul class="chatList">
+            <li class="botOutput botOutput--standard">Hello, I am chatbot 4</li>
+            <li class="botOutput botOutput--standard">I can help</li>
+            <li class="botOutput botOutput--standard">
+              <span class="botOutput--second-sentence">You can ask me anything</span>
+              <ul>
+                <li class="input__nested-list">User ID: {userID.users}</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
 
-          <div class="calculator">
-                <button class="calcButton calc1"> omega </button>
-                <button class="calcButton calc2"> theta </button>
-                <button class="calcButton calc3"> 3 </button>
-                <button class="calcButton calc4"> 4 </button>
-                <button class="calcButton calc5"> 5 </button>
-                <button class="calcButton calc6"> 6 </button>
-                <button class="calcButton calc7"> 7 </button>
-                <button class="calcButton calc8"> 8 </button>
-                <button class="calcButton calc9"> 9 </button>
-                <button class="calcButton calc10"> 10 </button>
-          </div>
+        <div class="calculator">
+          <button class="calcButton calc1"> omega </button>
+          <button class="calcButton calc2"> theta </button>
+          <button class="calcButton calc3"> 3 </button>
+          <button class="calcButton calc4"> 4 </button>
+          <button class="calcButton calc5"> 5 </button>
+          <button class="calcButton calc6"> 6 </button>
+          <button class="calcButton calc7"> 7 </button>
+          <button class="calcButton calc8"> 8 </button>
+          <button class="calcButton calc9"> 9 </button>
+          <button class="calcButton calc10"> 10 </button>
+        </div>
 
-          <div class="chatboxArea">
-            <div>
-              <form action="" id="chatform" onSubmit={handleSubmit}>
-                <textarea placeholder="..." class="chatbox" name="chatbox" onChange={event => setText(event.target.value)} minLength="2" style={{ resize: "none" }}></textarea>
+        <div class="chatboxArea">
+          <div>
+            <form action="" id="chatform" onSubmit={handleSubmit}>
+              <input placeholder="..." class="chatbox" name="chatbox" onChange={event => setText(event.target.value)} minLength="2" style={{ resize: "none" }}/>
                 <input class="submit-button" type="submit" value="send" style={{ resize: "none" }} />
-              </form>
-            </div>
+            </form>
           </div>
         </div>
+      </div>
 
     </body>
    
